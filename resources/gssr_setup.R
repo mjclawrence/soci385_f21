de319d51f2da1042c458632b6f2b80e5e317a2b6
mjclawrence <32300703+mjclawrence@users.noreply.github.com>
2021-09-21 17:42:09 -0400

    Drop NAs from assignment_02

--- resources/gssr_setup.R
+++ resources/gssr_setup.R
@@ -4,10 +4,24 @@ data("gss_doc")
 data("gss_all")
 
 
+gss_doc %>% filter(id == "educ") %>%
+  select(properties) %>%
+  unnest(cols = c(properties))
+
+
+gss_myvars <- gss_all %>%
+  select(id, year, educ, agekdbrn)
+
+summary(gss_myvars)
+
+gss_doc %>% filter(id == "agekdbrn") %>%
+  select(description)
 
 gss_subset <- gss_all %>%
-  filter(year>=2008 & !is.na(agekdbrn)) %>%
+  filter(year>=2008 & !is.na(agekdbrn) & !is.na(educ)) %>%
   select(id, year, educ, agekdbrn)
+
+summary(gss_subset)
 
 write.csv(gss_subset, "data/assignment_02.csv", row.names = FALSE)
 
